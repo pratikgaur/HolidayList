55b5fe99a2af45f9fd1f64148792ba2588433557
RuthvikC95 <54645098+RuthvikC95@users.noreply.github.com>
2019-11-17 21:35:33 -0500

    Code Review changes

--- HolidayList/Pages/Index.cshtml.cs
+++ HolidayList/Pages/Index.cshtml.cs
@@ -11,69 +11,69 @@ using QuickTypePlaces;
 namespace HolidayList.Pages
 {
     public class IndexModel : PageModel
-    {
-        //WebClient webClient = null;
+    {        
         public JsonResult OnGet()
         {
-            using (WebClient webClient = new WebClient())
-            {
-                String Places = webClient.DownloadString("https://pkgstore.datahub.io/core/world-cities/world-cities_json/data/5b3dd46ad10990bca47b04b4739a02ba/world-cities_json.json");
-                String jsonString = webClient.DownloadString("https://calendarific.com/api/v2/holidays?&api_key=3b2cc4a9fcb58a121947815135fca92694b0f63e&country=US&year=2019");
-                Places[] Place = QuickTypePlaces.Places.FromJson(Places);
-                
-                QuickType.Welcome welcome = QuickType.Welcome.FromJson(jsonString);
-                List <QuickType.Holiday> holidays = new List<Holiday>();
-                string state = null;
-                foreach (QuickTypePlaces.Places Area in Place)
+                // Property Initialization and parsing of JSON data
+                String state = null;
+                String filterid = null;
+                List <QuickType.Holiday> stateHolidays = new List<Holiday>();
+                String cityData = GetData("https://pkgstore.datahub.io/core/world-cities/world-cities_json/data/5b3dd46ad10990bca47b04b4739a02ba/world-cities_json.json");
+                String holidayData = GetData("https://calendarific.com/api/v2/holidays?&api_key=3b2cc4a9fcb58a121947815135fca92694b0f63e&country=US&year=2019");
+                QuickTypePlaces.Places[] allCities = QuickTypePlaces.Places.FromJson(cityData);
+                QuickType.Welcome allHolidays = QuickType.Welcome.FromJson(holidayData);                
+                                                        
+                //Choosing "New york" as an example to display the list of holidays for the state
+                foreach (QuickTypePlaces.Places city in allCities)
                 {
-                    if(Area.Country == "United States" && Area.Subcountry == "New York")
+                    if(city.Country == "United States" && city.Subcountry == "New York")
                     {
-
-                        state = Area.Subcountry;
+                        state = city.Subcountry;
                         break;
                     }
+
                 }
 
-
-                foreach (QuickType.Holiday Holidays in welcome.Response.Holidays)
+                //Populating the list of holidays for New York
+                foreach (QuickType.Holiday Holiday in allHolidays.Response.Holidays)
                 {
-                   if (Holidays.States.Enum == null)
-                    {
-                        if ((Holidays.States.StateArray[0].Name) == state)
+                   // Adding the state-specific holidays to the list
+                   if (Holiday.States.Enum == null)
+                   {
+                        if (((Holiday.States.StateArray[0].Name) == state) && (filterid == "Null"))
                         {
-                            holidays.Add(Holidays);
+                            stateHolidays.Add(Holiday);
+                            filterid = Holiday.States.StateArray[0].Iso;
                         }
-                         
-                    }
-                    else
-                    {
-                        holidays.Add(Holidays);
-                    }
+
+                        else if ((Holiday.States.StateArray[0].Name) == state)
+                        {
+                            stateHolidays.Add(Holiday);
+                        }
+    
+                   }
+                   // Adding national holidays to the list
+                  else
+                  {
+                        stateHolidays.Add(Holiday);
+                  }
                    
-                }
+                }                                                                       
+                //returning the filtered list of holidays for New York in JSON format
+                String filteredHolidaysList = GetData("https://calendarific.com/api/v2/holidays?&api_key=3b2cc4a9fcb58a121947815135fca92694b0f63e&country=US&year=2019&location="+ filterid);
+                return new JsonResult(QuickType.Welcome.FromJson(filteredHolidaysList));                                 
+        }
 
-                ViewData["Holidays"] = holidays;
-                ViewData["state"] = state;
-                ViewData["Places"] = Place;
+        //Downloading JSON data from APIs
+        public string GetData(string endpoint)
+        {
+            string downloadedData = "";
+            using (WebClient webClient = new WebClient())
+            {
+                downloadedData = webClient.DownloadString(endpoint);
 
-                String filterid = null;
-                foreach (QuickType.Holiday Holidays in welcome.Response.Holidays)
-                {
-                    if (Holidays.States.Enum == null)
-                    {
-                        if ((Holidays.States.StateArray[0].Name) == state)
-                        {
-                            filterid = Holidays.States.StateArray[0].Iso;
-                        }
-                    }
-                }
-
-                String filteredHolidaysList = webClient.DownloadString("https://calendarific.com/api/v2/holidays?&api_key=3b2cc4a9fcb58a121947815135fca92694b0f63e&country=US&year=2019&location="+ filterid);
-                QuickType.Welcome StateHoliday = QuickType.Welcome.FromJson(filteredHolidaysList);
-
-                return new JsonResult(StateHoliday);
-
-            }             
+            }
+            return downloadedData;
         }
     }
 }
